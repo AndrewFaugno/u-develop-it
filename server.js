617eff57780486f26608327f7bdd3e13d6309ec1
Andrew Faugno <andrewfaugno825@gmail.com>
2022-06-08 18:18:12 -0400

    linked database to server with mysql2

--- server.js
+++ server.js
@@ -2,15 +2,34 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require('mysql2');
+
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World!'
     });
 });
+
+db.query(`Select * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // Default response for any other request (Not Found)       (always goes last to not override other routes)
 app.use((req, res) => {
