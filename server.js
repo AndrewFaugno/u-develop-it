95fea001e2285830f5981d171b5b544856204f52
AndrewFaugno <andrewfaugno825@gmail.com>
2022-06-12 14:00:58 -0400

    created get and delete server routes for parties data table

--- server.js
+++ server.js
@@ -57,9 +57,38 @@ app.get('/api/candidates/:id', (req, res) => {
     LEFT JOIN parties
     ON candidates.party_id = parties.id 
     WHERE candidates.id = ?`;
-    
+
     const params = [req.params.id];
 
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success',
+            data: row
+        });
+    });
+});
+
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/parties/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
     db.query(sql, params, (err, row) => {
         if (err) {
             res.status(400).json({ error: err.message });
@@ -83,6 +112,27 @@ app.delete('/api/candidate/:id', (req, res) => {
         } else if (!result.affectedRows) {
             res.json({
                 message: 'Canidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+app.delete('/api/parties/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ message: res.message });
+            // checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
             });
         } else {
             res.json({
