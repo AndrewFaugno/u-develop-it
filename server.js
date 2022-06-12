8e96d25b3ee0f1cb0942ef4cfb6c3db3c51fe7cf
AndrewFaugno <andrewfaugno825@gmail.com>
2022-06-11 20:28:41 -0400

    joined party name with candidate tables and updated server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection(
         // MySQL username
         user: 'root',
         // MySQL password
-        password: 'Pseudoducks123$',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
@@ -31,7 +31,11 @@ app.get('/', (req, res) => {
 
 // GET all candidates 
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id`;
     
     db.query(sql, (err, rows) => {
         if (err) {
@@ -47,7 +51,13 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate
 app.get('/api/candidates/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates 
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
+    
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
