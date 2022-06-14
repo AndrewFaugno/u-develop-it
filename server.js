21bd515232194f55aa448fc487a0bbf5cbaddbb1
AndrewFaugno <andrewfaugno825@gmail.com>
2022-06-14 16:14:51 -0400

    saving place

--- server.js
+++ server.js
@@ -72,32 +72,56 @@ app.get('/api/candidates/:id', (req, res) => {
     });
 });
 
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    db.query(sql, (err, rows) => {
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if(errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?, ?, ?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
         if (err) {
-            res.status(500).json({ error: err.message });
+            res.status(400).json({ error: err.message});
             return;
         }
         res.json({
             message: 'Success',
-            data: rows
+            data: body
         });
     });
 });
 
-app.get('/api/parties/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
+// Update a candidate's party
+app.put('/api/candidates/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    } 
+    
+    const sql = `UPDATE candidates SET party_id = ?
+                WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
         if (err) {
             res.status(400).json({ error: err.message });
-            return;
+            // check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate ot found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
         }
-        res.json({
-            message: 'Success',
-            data: row
-        });
     });
 });
 
@@ -123,6 +147,38 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
 });
 
+// GET all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success',
+            data: rows
+        });
+    });
+});
+
+// GET a single party
+app.get('/api/parties/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success',
+            data: row
+        });
+    });
+});
+
+// DELETE a single party
 app.delete('/api/parties/:id', (req, res) => {
     const sql = `DELETE FROM parties WHERE id = ?`;
     const params = [req.params.id];
@@ -141,29 +197,6 @@ app.delete('/api/parties/:id', (req, res) => {
                 id: req.params.id
             });
         }
-    });
-});
-
-// Create a candidate
-app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if(errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?, ?, ?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message});
-            return;
-        }
-        res.json({
-            message: 'Success',
-            data: body
-        });
     });
 });
 
